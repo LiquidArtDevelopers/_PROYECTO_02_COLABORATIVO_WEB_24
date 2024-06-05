070592e1b07dfe1bb45a5ba563f3cd77f2c02414
LiquidArtDevelopers <aranaz@gmail.com>
2024-06-05 13:52:51 +0200

    EVENTO SCRCOLL

--- assets/js/_scroll.js
+++ assets/js/_scroll.js
@@ -1,18 +1,25 @@
 
+//cogemos arios elementos del artículo 6
+const elem01 = document.getElementById('elementoAdetectar01');
+const elem02 = document.getElementById('elementoAdetectar02')
+//navegador
+const navegador = document.getElementById("navegador")
+
+
 // cuando se detecte movimiento de scroll llamamos a la función scrollFunction
 window.onscroll=function(){
-    mifuncion()
+    cambiarNav()
+    animaciones()
 };
 
-//función a la que sólo entraremos cuando esta sea llamada desde el evento
-function mifuncion() {
 
-    const navegador = document.getElementById("navegador")
+//función a la que sólo entraremos cuando esta sea llamada desde el evento
+function cambiarNav() {    
         
     //si el top del scroll del body es inferior a 80 de posición, 
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 
-        navegador.style.backgroundColor = "pink";
+        navegador.style.backgroundColor = "rgba(20, 20, 20, 0.5)";
         navegador.style.height="50px";
         
         /* for(const item of document.getElementsByTagName("a")){
@@ -20,7 +27,7 @@ function mifuncion() {
             item.style.transition="1s";
         }  */       
 
-    } else {
+    }else{
 
         navegador.style.backgroundColor = "rgb(216, 216, 255)";
         navegador.style.height="100px";
@@ -30,4 +37,36 @@ function mifuncion() {
             item.style.transition="1s";
         } */
     }
+}
+
+function animaciones(){
+    //vemos si el elemento 01 está o no dentro del viewport
+    if (estaEnlaPantalla(elem01)==true) {
+        // está en el viewport
+        console.log("Aparece");
+        elem01.classList.remove("desaparecerHaciaDer")
+        elem01.classList.add("aparecerDer")        
+    }else{
+        //no está en el viewport
+        elem01.classList.remove("aparecerDer")
+        elem01.classList.add("desaparecerHaciaDer")
+    }
+
+    if (estaEnlaPantalla(elem02)==true) {
+        // está en el viewport
+        console.log("Aparece");
+        elem02.classList.remove("desaparecerHaciaIzd")
+        elem02.classList.add("aparecerIzd")        
+    }else{
+        //no está en el viewport
+        elem02.classList.remove("aparecerIzd")
+        elem02.classList.add("desaparecerHaciaIzd")
+    }
+
+
+}
+
+function estaEnlaPantalla(parametroRecibido) {
+    var distance = parametroRecibido.getBoundingClientRect();
+    return (distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0);
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import "./_toggle.js";
 import "./_scroll.js";
 
+
 const midocumentoHTML = document;
 midocumentoHTML.addEventListener('DOMContentLoaded',()=>{ 
     
