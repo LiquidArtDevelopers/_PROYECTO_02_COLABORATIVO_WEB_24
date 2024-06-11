8a5b7ea025fae62b0da646c4c331364cbff977d2
sevianamaria <ana190483@hotmail.com>
2024-06-11 13:58:48 +0200

    toggle

---
+++ assets/js/ana.js
@@ -0,0 +1,32 @@
+//cogemos el navegador para el efecto del nav
+const navegador = document.getElementById("navegador")
+
+//listener de scroll para modificar el nav
+window.onscroll=function(){
+    cambiarNav()
+}
+//función a la que sólo entraremos cuando esta sea llamada desde el evento
+function cambiarNav() {    
+        
+    //si el top del scroll del body es inferior a 80 de posición, 
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+
+        navegador.style.backgroundColor = "#b37bad29";
+        navegador.style.height="50px";
+        
+        /* for(const item of document.getElementsByTagName("a")){
+            item.style.color="pink";            
+            item.style.transition="1s";
+        }  */       
+
+    }else{
+
+        navegador.style.backgroundColor = "rgba(9, 9, 9, 1)";
+        navegador.style.height="120px";
+
+        /* for(const item of document.getElementsByTagName("a")){
+            item.style.color="white";
+            item.style.transition="1s";
+        } */
+    }
+}
